a268586fe7802483566ae563aab16f81ccc1224d
akhror-valiev <ahror.valiev@gmail.com>
2022-11-24 08:02:31 +0500

    Fix linter error

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import './style.css';
 // eslint-disable-next-line import/no-cycle
 import { sendData, fetchData } from './projectApi.js';
 
-export const scoreList = document.querySelector('.score-list');
+const scoreList = document.querySelector('.score-list');
 const inputName = document.querySelector('.name');
 const inputScore = document.querySelector('.score');
 const refBtn = document.querySelector('.btn');
 const btnForm = document.querySelector('.btn-form');
 
-export const populateList = (data) => {
+const populateList = (data) => {
   data.forEach((n) => {
     const textHtml = `<p class="score-num">${n.user}: ${n.score}</p>`;
     scoreList.insertAdjacentHTML('afterbegin', textHtml);
@@ -33,3 +33,4 @@ window.onload = () => {
     fetchData();
   });
 };
+export { populateList, sendData };
--- src/projectApi.js
+++ src/projectApi.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line import/no-cycle
 import { populateList } from './index.js';
 
 const renderInfo = document.querySelector('.render-info');
